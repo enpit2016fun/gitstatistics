ab299882ce5ba2fb59b18008ab3a213fb4d2fbe8
Katsuya Matsubara <matsu@fun.ac.jp>
2018-08-15 12:09:30 +0900

    Customize the gitstat to fix some graphs for the jssst paper

--- GenStats.sh
+++ GenStats.sh
@@ -6,10 +6,10 @@ for team in a b c d e f
 do
   repo=minipbl_$team
   if [ ! -d ${repo} ]; then
-    git clone git://github.com/enpit2016fun/${repo}.git
+    echo ${repo} not found.
+    exit 1
   fi
-  ( cd ${repo} && git pull --all --force ) || exit 1
-  gitstats ${repo} stats_$team
+  ./gitstats ${repo} stats_$team
 done
 
 commitdate=`LANG=C date`
@@ -18,6 +18,6 @@ sed -i -e "/Last Update/c\\<div align=\"right\"\>Last Update:\ $commitdate\</div
 
 git add index.html activity.html stats_*
 git commit -m "Update $commitdate"
-#git push origin gh-pages
+git push origin gh-pages
 
 git checkout master
